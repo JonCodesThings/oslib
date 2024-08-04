17bb2e459ac32a8dc5b5d517de5e20f891743a9d
JonCodesThings <jon@joncodesthings.net>
2024-08-04 08:15:31 +0100

    Made GetFilesWithExtensionInDirectory return const

--- include/oslib/io.h
+++ include/oslib/io.h
@@ -15,7 +15,7 @@ i32 OSLIB_WriteBytesToFile(const char *filepath, i8 *buffer, i32 bufferSize);
 
 i32 OSLIB_AppendBytesToFile(const char *filepath, i8 *buffer, i32 bufferSize);
 
-char ** OSLIB_GetFilesWithExtensionInDirectory(const char *filepath, const char *extension);
+const char ** OSLIB_GetFilesWithExtensionInDirectory(const char *filepath, const char *extension);
 
 i32 OSLIB_DeleteFile(const char* filepath);
 
